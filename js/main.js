60d0effc241d4162e1f62eb0faf74c1aac455cbc
MostafaHusnay <mostafa.husany@gmail.com>
2018-07-16 16:33:24 +0200

    16/7/2018

--- js/main.js
+++ js/main.js
@@ -1,4 +1,82 @@
 $(function() {
 	
+	// Dynamic resizing
+	$(window).on("resize", function() {
+		
+		// Header Resizing
+		$(".header").css("height", $(window).height());
+		
+		// Related to responsivd navbar display
+		if($(this).width() > 900) {
+			$(".navbar .links-wraper").css("display", "block");
+		}
+		else {
+			$(".navbar .links-wraper").css("display", "none");
+		}
+		
+		// Adjust Bxslider List Item Center
+		$(".bxslider").each(function() {
+			var height =  ($(window).height() - $(".bxslider li").height())/2;
+			$(this).css("paddingTop", height);
+		});
+	});
 	
+	// Header Resizing
+	$(".header").css("height", $(window).height());
+	
+	// Activating bxslider
+	var slider = $(".bxslider").bxSlider({
+		pager:false,
+	});
+	slider.startAuto();
+	
+	// Adjust Bxslider List Item Center
+	$(".bxslider").each(function() {
+		var height =  ($(window).height() - $(".bxslider li").height())/2;
+		$(this).css("paddingTop", height);
+	});
+	
+	// Link active action 
+	$(".navbar .links li").on("click", function() {
+		$(this).addClass("active").siblings().removeClass("active");
+	});
+	
+	// Link-i click action
+	$(".navbar .links-i").on("click", function() {
+		$(".navbar .links-wraper").toggle(1000);
+	});
+	
+	// Smooth Scroll
+	$(".navbar li").on("click", function() {
+		var $sectionSelctor = $($(this).data("scroll"));
+		$("html, body").animate({
+			scrollTop: $sectionSelctor.offset().top //- 50
+		}, 600);
+	});
+	
+	
+	/*
+	var flag = true;
+	$(window).on("scroll", function() {
+		if ($(this).scrollTop() > 100) {
+			if (flag){
+				$(".navbar").css({position:"fixed",
+										width: "100%",
+										top: 0,
+										background:"rgba(0,0,0,0.5)"
+									}).fadeIn(1000).children().children().css("margin", "10px 0");
+				flag = false;
+			}
+			
+		} else {
+			$(".navbar").css({position:"relative",
+									width: "100%",
+									top: 0,
+									background:"rgba(0,0,0,0)"
+								});
+			flag = true;
+		}
+		
+	});
+	*/
 });
